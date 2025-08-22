e6cb4ae19fcb16dd707b6eaa21bbffedbaeed393
Widy Guilias <widyguilias@gmail.com>
2025-08-22 11:26:58 +0700

    add new method getFutureNotification

--- controllers/notification.controller.js
+++ controllers/notification.controller.js
@@ -1,5 +1,7 @@
-const db = require('../models')
-const Notification = db.notification
+const database_POS = require('../databases/database_POS');
+const db = require('../models');
+const Notification = db.notification;
+require('dotenv').config();
 
 exports.getReport = async (req, res) => {
 	const dateFrom = req.body.startDate
@@ -27,4 +29,35 @@ exports.getReport = async (req, res) => {
 			message: error.message || "some error while showing notification report"
 		})
 	}
+}
+
+exports.getFutureNotification = async (req, res) => {
+	const dateFrom = req.body.startDate
+	const dateTo = req.body.endDate
+
+	try{
+		console.log("start to query to database POS to get future data notification")
+
+		let result = await database_POS.query(
+		  `SELECT s.date AS payment_date, c.customer_name , c.customer_email, c.customer_phone FROM sales s JOIN customers c on s.customer_id = c.id WHERE s.date = DATE_FORMAT(DATE_SUB(CONVERT_TZ(:dateFrom, '+00:00', '+07:00'), INTERVAL ${process.env.INTERVAL_DAYS} DAY), '%Y-%m-%d');`,
+		  { raw: true, type: database_POS.QueryTypes.SELECT, replacements: { dateFrom }} 
+		)
+
+		console.log("get future data notification POS successfully")
+
+		if(!result){
+			res.status(404).send({
+				message: "future notification not found"
+			})
+		}
+		res.send(result)
+
+	} catch(error){
+		res.status(409).send({
+			message: error.message || "some error while showing future notification report"
+		})
+	}
+
+
+
 }
